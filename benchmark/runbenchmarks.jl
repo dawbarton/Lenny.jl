a2dd39ed5581d92a1ed9269ab5beb4bd61cc31c3
David AW Barton <david.barton@bristol.ac.uk>
2018-07-09 14:37:23 +0100

    Made saving of benchmark results optional

--- benchmark/runbenchmarks.jl
+++ benchmark/runbenchmarks.jl
@@ -5,11 +5,18 @@ results = benchmarkpkg(joinpath(@__DIR__, "..", "."))
 
 const resultsfile = joinpath(@__DIR__, "results.jld2")
 
-if isfile(resultsfile)
-    @load "$resultsfile" savedresults
-    push!(savedresults, results)
-    @save "$resultsfile" savedresults
-else
-    savedresults = [results]
-    @save "$resultsfile" savedresults
+if !isdefined(current_module(), :saveresults)
+    saveresults = true
+end
+
+if saveresults
+    if isfile(resultsfile)
+        @load "$resultsfile" savedresults
+        push!(savedresults, results)
+        @save "$resultsfile" savedresults
+    else
+        savedresults = [results]
+        @save "$resultsfile" savedresults
+    end
+    println("Results saved to disk")
 end
