f7e6153082496f8660eea817a580ced9f64ee925
David AW Barton <david.barton@bristol.ac.uk>
2018-07-14 09:36:32 +0100

    Updated callbacks to the new scheme

--- src/Callbacks.jl
+++ src/Callbacks.jl
@@ -1,15 +1,36 @@
 module Callbacks
 
-export After, Before, emit!
+export CallbackList, checkallowable, addcallback!, getcallbacks
 
-struct After{T}
-    func::T
+const CallbackName = String
+
+struct CallbackList
+    allowable::Vector{CallbackName}
+    callbacks::Dict{CallbackName, Vector{Any}}
 end
 
-struct Before{T}
-    func::T
+function checkallowable(cblist::CallbackList)
+    for key in keys(cblist.callbacks)
+        if !(key in cblist.allowable)
+            throw(InvalidStateException("Callback does not correspond to a registered signal - $key"))
+        end
+    end
+    true
 end
 
-emit!(prob, callback) = nothing
+function addcallback!(cblist::CallbackList, name::CallbackName, callback)
+    if !(name in keys(cblist.callbacks))
+        cblist.callbacks[name] = Vector{Any}()
+    end
+    if callback in cblist.callbacks[name]
+        throw(ArgumentError("Callback has already been added to that signal - $name"))
+    end
+    push!(cblist.callbacks[name], callback)
+    cblist
+end
+
+function getcallbacks(cblist::CallbackList, name::CallbackName)
+    (cblist[name]...,)
+end
 
 end
