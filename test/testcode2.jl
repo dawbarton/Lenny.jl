13628855b7a3d21fae86e7666050bc456f3b6eaf
David AW Barton <david.barton@bristol.ac.uk>
2018-08-04 00:32:46 +0100

    Updated testcode2

--- test/testcode2.jl
+++ test/testcode2.jl
@@ -6,8 +6,8 @@ function plane!(res, x, z)
     res[1] = x[1] + z[1]
 end
 
-function hyper!(res, x, z)
-    res[1] = x[1]^2 - z[1]^2
+function hyper!(x, z)
+    x[1]^2 - z[1]^2
 end
 
 x = EmbeddedFunctions.StateVar("x", [0.9])
@@ -16,12 +16,12 @@ z = EmbeddedFunctions.StateVar("z", [-1.1])
 
 z1 = EmbeddedFunctions.ZeroFunction(circle!, [x, y], 1)
 z2 = EmbeddedFunctions.ZeroFunction(plane!, [x, z], 1)
-z3 = EmbeddedFunctions.ZeroFunction(hyper!, [x, z], 1)
+m1 = EmbeddedFunctions.MonitorFunction(hyper!, [x, z], "hyper", true)
 
-cl = EmbeddedFunctions.ClosedEmbeddedFunctions([z1, z2, z3], Vector{EmbeddedFunctions.MonitorFunction{Float64, F} where F}())
+cl = EmbeddedFunctions.ClosedEmbeddedFunctions([z1, z2], [m1])
 
-u = zeros(3)
-EmbeddedFunctions.pullu!(u, cl)
+u = zeros(4)  # includes continuation parameters (μ)
+EmbeddedFunctions.pullu!(u, cl)  # doesn't include continuation parameters *at the moment*
 
 res = zeros(3)
 EmbeddedFunctions.rhs!(res, cl, u)
